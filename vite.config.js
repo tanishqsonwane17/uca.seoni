b9871958d68deb85e18a3df4972ae74a64170561
Tanishq Sonwane <your-email@example.com>
2025-05-17 15:33:30 +0530

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
     react(),
-     tailwindcss(),
+    tailwindcss(),
   ],
-})
+  server: {
+    // 👇 This is the fix for 404 on refresh
+    historyApiFallback: true,
+  },
+});
