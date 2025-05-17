67921f1ff6e173e710064d383616cb5d5221d276
Tanishq Sonwane <your-email@example.com>
2025-05-17 15:27:49 +0530

    Msc added

--- src/controller/Courses.jsx
+++ src/controller/Courses.jsx
@@ -33,21 +33,21 @@ export default function Courses() {
         id:'Pgdca-details'
       },
       {
-        title: "PGDCA",
+        title: "MSC",
         tags: ["ON CAMPUS","HINDI + ENGLISH"],
         price: 36000,
         oldPrice: 49000,
         discount: "50% OFF",
         badge: "2 YEARS",
-        image: "https://imgs.search.brave.com/u-PzClkofDHSaphmUaGgy0JdY7fQYYyAnvpkOWHahVY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pY2Rk/ZS5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMjEvMDMvUEct/RGlwbG9tYS1pbi1D/b21wdXRlci1BcHBs/aWNhdGlvbi1EaXN0/YW5jZS1MZWFybmlu/Zy5qcGc",
-        id:'Pgdca-details'
+        image: "https://www.bhartieduversity.com/assets/images/courses/msc.png",
+        id:'Msc-details'
       },
     ];
   
     return (
       <>
       <div className="bg-[#9cf6fb] min-h-screen  py-10 px-4">
-        <img src='' alt="Universal" />
+        <img className="" src='' alt="universal"></img>
         <h2 className="text-2xl font-semibold mb-6 mt-[2vw]">Courses which do work ↓</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-16  w-[90vw] mx-[5vw]">
           {courses.map((course, index) => (
