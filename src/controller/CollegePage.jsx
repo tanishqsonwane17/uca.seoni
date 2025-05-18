bdd6a810105e3e7bc5429053056f77e6b79e8b5d
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-05-18 08:36:04 +0530

    working-well

--- src/controller/CollegePage.jsx
+++ src/controller/CollegePage.jsx
@@ -76,7 +76,7 @@ const CollegePage = () => {
                   alt={img.text}
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-60 transition duration-300 flex items-center justify-center">
+                <div className="absolute inset-0 bg-opacity-50 group-hover:bg-opacity-60 transition duration-300 flex items-center justify-center">
                   <p className="text-white text-xl sm:text-2xl font-semibold text-center px-4">
                     {img.text}
                   </p>
